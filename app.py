85d2b04e51fe24c6ab9b57372e7536e03a4da62a
Joris <jote0002@stud.kea.dk>
2025-10-31 09:53:01 +0100

    git init

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_session import Session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+import x
+import time
+import uuid
+import os
+
+from icecream import ic
+ic.configureOutput(prefix=f'----- | ', includeContext=True)
+
+app = Flask(__name__)
+
+# Set the maximum file size to 10 MB
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024   # 1 MB
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+ 
+
+##############################
+##############################
+##############################
+def _____USER_____(): pass 
+##############################
+##############################
+##############################
+
+@app.get("/")
+def view_index():
+   
+    return render_template("index.html")
+
+@app.get("/home")
+def view_home():
+    return render_template("home.html")
